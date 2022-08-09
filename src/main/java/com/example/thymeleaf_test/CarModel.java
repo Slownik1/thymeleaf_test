7cd80e0f20baed983c41490d95f8a998a4d7e1c8
dawid białek <dawid1bialek@gmail.com>
2022-08-09 21:47:16 +0200

    Add Car model

---
+++ src/main/java/com/example/thymeleaf_test/CarModel.java
@@ -0,0 +1,24 @@
+package com.example.thymeleaf_test;
+
+
+public class CarModel {
+
+    private String model;
+    private String mark;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+}
--- src/main/java/com/example/thymeleaf_test/HelloController.java
+++ src/main/java/com/example/thymeleaf_test/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.thymeleaf_test;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello(Model model){
+        model.addAttribute("name", "Dawid");
         return "hello";
     }
 
